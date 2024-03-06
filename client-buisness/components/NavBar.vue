794f834737c2a86ba1963eeb8ed83dcc3058a336
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-03-06 11:40:47 -0500

    feat: Creating NavBar component

---
+++ client-buisness/components/NavBar.vue
@@ -0,0 +1,82 @@
+<template>
+  <nav
+    class="flex items-center justify-between h-16 bg-gray-800 text-white px-4 md:px-8"
+  >
+    <nuxt-link class="text-xl font-bold">Business App</nuxt-link>
+
+    <ul class="hidden md:flex space-x-6">
+      <li>
+        <nuxt-link
+          class="hover:cursor-pointer"
+          v-bind:style="{ color: 'inherit' }"
+        >
+          Home
+        </nuxt-link>
+      </li>
+      <li>
+        <button @click="toggleMenu">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16v12a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+            />
+          </svg>
+        </button>
+      </li>
+    </ul>
+  </nav>
+
+  <div
+    v-show="showMenu"
+    class="fixed inset-0 bg-gray-800 opacity-75 backdrop-blur transition duration-300 ease-in-out z-50"
+  >
+    <ul
+      class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center space-y-4 text-center text-xl font-bold text-white"
+    >
+      <li @click="toggleMenu">
+        <nuxt-link to="/">Products</nuxt-link>
+      </li>
+      <li @click="toggleMenu">
+        <nuxt-link to="/">Sells</nuxt-link>
+      </li>
+      <li @click="toggleMenu">
+        <nuxt-link to="/">Expenses</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
+    },
+  },
+};
+</script>
