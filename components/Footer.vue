16c63efa35b8a6cf591eb460accb7f50e651079d
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-03-13 13:36:24 -0400

    feat: add Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="bg-gray-100 py-4">
+    <div class="container mx-auto text-center">
+      <p>Copyright &copy; 2024 - Todos los derechos reservados.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
