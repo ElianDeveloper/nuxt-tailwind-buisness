e969df2547dafccbe6eb5f3b6faf06fe276f8e45
ElianDeveloper <marincabreraeliandaniel@gmail.com>
2024-03-13 13:36:49 -0400

    feat: add products page

---
+++ pages/products.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex row justify-between">
+    <h1 class="text-xl font-bold">List Products</h1>
+    <AddProducts />
+  </div>
+  <CardProduct />
+</template>
+
+<script>
+export default {
+  name: "products",
+  data() {
+    return {};
+  },
+};
+</script>
